112b4d887ea5654293201b45eb8861fa979e554f
Juan Daniel Rubio Camacho <danirubio780@hotmail.com>
2020-05-06 02:16:19 +0000

    Practica 10

---
+++ threads/youtube.c
@@ -0,0 +1,48 @@
+#include <pthread.h> 
+#include <stdio.h>  
+#include <stdlib.h>  
+#include <semaphore.h>
+
+#define N 20
+
+sem_t buffer_mutex;
+char buffer[N+1];
+
+sem_t empty;
+sem_t full;
+
+void *producer(void * th){
+    for (int i = 0; i < 5*N; i++){
+        sem_wait(&empty);
+        sem_wait(&buffer_mutex);
+        buffer[(i) % N] = 'a';
+        printf("\n %s \n", buffer);
+        sem_post(&buffer_mutex);
+        sem_post(&full);
+    }
+}
+
+void *consumer(void * th){
+    for (int i = 0; i < 5*N; i++){
+        sem_wait(&full);
+        sem_wait(&buffer_mutex);
+        printf("%c ", buffer[(i) % N]);
+        buffer[(i) % N] = '-';
+        sem_post(&buffer_mutex);
+        sem_post(&empty);
+
+    }
+}
+
+int main(){
+    buffer[N] = '\0';
+    pthread_t tp, tc;
+    sem_init(&buffer_mutex, 0, 1);
+    sem_init(&empty, 0, N);
+    sem_init(&full, 0, 0);
+    pthread_create(&tp, NULL, producer, NULL);
+    pthread_create(&tc, NULL, consumer, NULL);
+    pthread_exit(NULL);
+
+}
+
